1f5cfa5a8457e63bcd7433a844e92ccb20136354
23-jose <jose.miguel.b.antunes@gmail.com>
2025-06-22 18:51:05 +0900

    Finished main page implementation, added logo to Navigation Bar, reinstalled yarn

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -3,7 +3,12 @@ import Link from 'next/link';
 export default function NavigationBar() {
     return (
         <nav className="h-[56px] bg-[#212121] text-white text-xl font-bold flex items-center justify-between pr-12 pl-12">
-            <Link href="/">BV</Link>
+            <div className="flex items-center">
+                <Link href="/" className="flex items-center space-x-2.5"> 
+                    <img src="/LogoCube.png" alt="LogoPicture" className="w-8 h-auto"/>
+                    <span>BV</span>
+                </Link>
+            </div>
             <div className="flex gap-16">
                 <Link href="/impact">Impact</Link>
                 <Link href="/ethereum">Ethereum</Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import NavigationBar from "../../components/NavigationBar";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-[#3A3A3A] text-white">
+    <div className="bg-[#3A3A3A] min-h-screen text-white">
       <NavigationBar></NavigationBar>
-      <div className="flex flex-col items-start justify-start px-16 py-16">
-        <div>Logo here</div>
-        <div className="font-bold">Welcome to Blockchain Visualiser</div>
-        <div className="font-semibold">
-          Select a blockchain, and visualise all its transactions,
-          or visit the impact page to see the environmental
+      <div className="w-full h-full flex flex-col items-start justify-start px-18 py-25">
+        <div><img src="/LogoCube.png" alt="LogoPicture" className="w-35 h-auto"/></div>
+        <div className="font-semibold pt-5 text-5xl" style={{ lineHeight: '1.2' }}>Welcome to<br />Blockchain Visualiser</div>
+        <div className="pt-3 text-2xl">
+          Select a blockchain, and visualise all its transactions,<br />
+          or visit the impact page to see the environmental<br />
           impact of these technologies
         </div>
       </div>
