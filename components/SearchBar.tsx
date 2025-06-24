d83a8aca7cb9a72b477fdfdbabaedc12297d4838
23-jose <jose.miguel.b.antunes@gmail.com>
2025-06-24 16:01:38 +0900

    Added SearchBar, started building Bitcoin, Ethereum and Impact pages' UI

---
+++ components/SearchBar.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState } from "react"
+import { IoSearch } from "react-icons/io5";
+
+export default function SearchBar() {
+
+    const [search, setSearch] = useState("");
+
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("Searching: ");
+    };
+
+    return (
+        <form onSubmit = {onSubmit} className="flex items-center border border-white rounded-md overflow-hidden max-w-md">
+            <input
+                type= "text"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search for a wallet"
+                className=""/>
+            <button type = "submit" className="">
+                <IoSearch className="flex h-6 w-6" />
+            </button>
+        </form>
+    );
+}
--- src/app/bitcoin/page.tsx
+++ src/app/bitcoin/page.tsx
@@ -1,9 +1,21 @@
 import NavigationBar from "../../../components/NavigationBar";
+import SearchBar from "../../../components/SearchBar";
 
 export default function BitcoinPage() {
   return (
     <div className="min-h-screen bg-[#3A3A3A] text-white">
       <NavigationBar></NavigationBar>
+      <div className="h-full w-full flex flex-col px-18 py-20">
+        <div className="text-5xl pb-4">Bitcoin</div>
+        <div className="flex items-center justify-between text-xl">
+          <div>Latest Transactions</div>
+          <div className="flex">
+            <div className="pr-15">
+              Filters Button
+            </div>
+            <SearchBar></SearchBar></div>
+        </div>  
+      </div>
     </div>
   );
 }
