660cf4d7c6d15259180f61a68bb136a5b7396f3c
23-jose <jose.miguel.b.antunes@gmail.com>
2025-07-12 12:10:44 +0900

    changed NavigationBar component to fit better on small screens

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -2,14 +2,14 @@ import Link from 'next/link';
 
 export default function NavigationBar() {
     return (
-        <nav className="h-[56px] bg-[#212121] text-white text-xl font-bold flex items-center justify-between pr-12 pl-12">
+        <nav className="h-[56px] bg-[#212121] text-white text-xl font-bold flex items-center justify-between px-4 md:px-12">
             <div className="flex items-center">
                 <Link href="/" className="flex items-center space-x-2.5"> 
                     <img src="/LogoCube.png" alt="LogoPicture" className="w-8 h-auto"/>
                     <span>BV</span>
                 </Link>
             </div>
-            <div className="flex gap-16">
+            <div className="flex gap-6 md:gap-16">
                 <Link href="/impact">Impact</Link>
                 <Link href="/ethereum">Ethereum</Link>
                 <Link href="/bitcoin">Bitcoin</Link>
