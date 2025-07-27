17ea14202540b688b65f9d8259a9c877f37ee97b
23-jose <jose.miguel.b.antunes@gmail.com>
2025-07-27 10:55:29 +0900

    fixed errors with impact card and filter component responsiveness on mobile

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -6,13 +6,11 @@ type FiltersProps = {
 
 export default function Filters({ onClose }: FiltersProps) {
   return (
-    <div className="fixed inset-0 backdrop-blur flex justify-center items-center px-75">
-      <div className="bg-[#2E2E2E] p-6 rounded-xl text-white w-full">
-        <div className="text-2xl items-center justify-center">Filters</div>
-
-
-        <div className="flex flex-col"></div>
-            <div className="flex justify-between gap-4 pt-5 pb-10 px-20">
+    <div className="fixed inset-0 backdrop-blur flex justify-center items-center px-4">
+      <div className="bg-[#2E2E2E] p-6 rounded-xl text-white w-full max-w-lg">
+        <div className="text-2xl items-center mb-6">Filters</div>
+        <div className="flex flex-col gap-6"></div>
+            <div className="flex flex-col sm:flex-row sm:justify-between s,:gap-4 gap-3">
                 <label className="flex items-center gap-2">
                     <input type="checkbox" className="accent-white" />
                     Water Usage
@@ -26,28 +24,14 @@ export default function Filters({ onClose }: FiltersProps) {
                     Electricity Spent
                 </label>
             </div>
-            <div className="flex justify-between gap-4 pt-5 pb-10 px-20">
-                <label className="flex items-center gap-2">
-                    <input type="checkbox" className="accent-white" />
-                    Water Usage
-                </label>
-                <label className="flex items-center gap-2">
-                    <input type="checkbox" className="accent-white" />
-                    Carbon Emissions
-                </label>
-                <label className="flex items-center gap-2">
-                    <input type="checkbox" className="accent-white" />
-                    Electricity Spent
-                </label>
-            </div>
-        <div className="flex justify-between">
+        <div className="flex justify-between gap-4 pt-6">
             <button
                 onClick={onClose}
-                className="mt-2 px-4 py-2 bg-white text-black rounded-md hover:bg-gray-300"
+                className="px-4 py-2 bg-white text-black rounded-md hover:bg-gray-300"
             >Close</button>
             <button
                 onClick={onClose}
-                className="mt-2 px-4 py-2 bg-white text-black rounded-md hover:bg-gray-300"
+                className="px-4 py-2 bg-white text-black rounded-md hover:bg-gray-300"
             >Apply</button>
         </div>
       </div>
--- components/ImpactCard.tsx
+++ components/ImpactCard.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function ImpactCard() {
   return (
-    <div className="bg-[#212121] rounded-2xl text-white w-1/3 h-100 flex flex-col justify-between">
+    <div className="bg-[#212121] rounded-2xl text-white h-100 flex flex-col justify-between">
         <div className="text-5xl pl-8 pt-8 max-w-xs">
             Energy Spent
         </div>
